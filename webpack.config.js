6acecd77dc9235a4fd632d5f4ceac1e74563e1de
soarflat <soarflat777@gmail.com>
2017-09-12 18:13:52 +0900

    [fix] JSをminifyしてビルドできなかったので修正

--- webpack.config.js
+++ webpack.config.js
@@ -19,11 +19,33 @@ module.exports = {
     }, {
       test: /\.vue$/,
       loader: 'vue-loader',
+      options: {
+        loaders: {
+          js: 'babel-loader?presets[]=env'
+        }
+      }
     }]
   },
   resolve: {
     alias: {
       vue: 'vue/dist/vue.js',
     }
-  }
+  },
 };
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        drop_console: true
+      },
+    }),
+  ]
+} else {
+  module.exports.devtool = '#source-map';
+}
